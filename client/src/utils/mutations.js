45df6b44d6e5c0b1a7ed568e4cfa436de30dea0a
Hailey <haileyacarlson@gmail.com>
2023-03-22 17:09:53 -0700

    Added ADD_USER, SAVE_BOOK, and REMOVE_BOOK to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,39 @@ export const LOGIN_USER = gql`
     }
   }
 `
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: ID!) {
+    saveBook(bookId: $bookId) {
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+`
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+`
